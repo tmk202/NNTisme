69126620d24584bf6eb36b75996f1affdf5cde90
tmk202 <giadung8686@gmail.com>
2026-04-21 00:51:13 +0700

    1

--- extract.py
+++
@@ -1,5 +0,0 @@
-import PyPDF2
-reader = PyPDF2.PdfReader('NNT_CV_Tech_vie.pdf')
-text = '\n'.join([page.extract_text() for page in reader.pages])
-with open('cv.txt', 'w', encoding='utf-8') as f:
-    f.write(text)
